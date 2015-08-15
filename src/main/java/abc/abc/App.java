d193ec7647f1ad82562b3f59d260da700e35b810
nainam <naina.motwani@tothenew.com>
2015-08-15 18:11:56 +0530

    First commit

---
+++ src/main/java/abc/abc/App.java
@@ -0,0 +1,18 @@
+package abc.abc;
+
+public class App 
+{
+    public String reverseString(String src) {
+
+	      return new StringBuilder(src).reverse().toString();
+
+	   }
+
+    public static void main( String[] args )
+    {
+        final String str = "Hello!";
+        App appObject = new App();
+        System.out.println("The reverse string of \"" + str + "\" is \"" +
+        appObject.reverseString(str) + "\"");
+    }
+}
